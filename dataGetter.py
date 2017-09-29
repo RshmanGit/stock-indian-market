64a18283af8e2bb65d2cc6f098d9686ea1cdf9bd
RshmanGit <aarsh.1903@yahoo.com>
2017-09-29 17:40:16 +0530

    Created the data generator class. Just input the ticker symbol in the constructor parameter and the data will be saved in the parent director

---
+++ dataGetter.py
@@ -0,0 +1,52 @@
+import quandl
+import datetime
+import pickle
+
+class dataGetter:
+
+    global key
+    global ticker
+
+    def __init__(self,ticker):
+        with open('../apiKey.txt','r') as keyfile:
+            self.key = keyfile.read().replace('\n','')
+        self.ticker = ticker
+
+    def getData(self):
+        global data
+        quandl.ApiConfig.api_key = self.key
+        date = datetime.datetime.now()
+        enddate = ("%s-%s-%s" % (date.year,date.month,date.day))
+        ticker = 'NSE/'+self.ticker
+        try:
+            print('[+] Quandl request')
+            self.data = quandl.get(ticker , start_date='2011-05-14', end_date=enddate)
+            print('[+] Quandl request pulled')
+
+        except:
+            print('[-] Quandl request failed')
+
+        try:
+            filename = '../'+self.ticker+'rawdata.p'
+            print('[+] Dumping data to pickle file: ')
+            pickle.dump(self.data, open(filename,'wb'))
+            print('[+] Dumping data success')
+
+        except:
+            print('[-] Dumping failed')
+
+    def retData(self):
+        filename = '../'+self.ticker+'rawdata.p'
+        data = []
+        try:
+            print('[+] Loading data from pickle file: ')
+            data = pickle.load(open(filename,'rb'))
+            print('[+] Data loaded')
+        except:
+            print('[-] Loading data failed')
+
+        return data
+
+abc = dataGetter('BSLGOLDETF')
+abc.getData()
+data = abc.retData()
